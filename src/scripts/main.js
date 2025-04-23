867b17673f15d0a2afad79dc7134f7e3d75dff78
Nikola57 <nikola.sribniy@gmail.com>
2025-04-23 17:17:34 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,9 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const items = document.querySelectorAll('.tree > li, .tree > li ul > li');
 
   items.forEach((item) => {
+    // Якщо <li> має вкладений <ul>, значить це категорія
     const nestedList = item.querySelector('ul');
 
     if (nestedList) {
+      // Витягаємо тільки текстовий вузол перед <ul>
       const firstChild = item.firstChild;
 
       if (firstChild.nodeType === Node.TEXT_NODE) {
@@ -23,6 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+  // ===== 2. Встановлюємо обробник кліку
   const tree = document.querySelector('.tree');
 
   tree.addEventListener('click', (eventt) => {
@@ -35,26 +38,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (childUl) {
       childUl.hidden = !childUl.hidden;
-    }
-  });
-
-  items.forEach((item) => {
-    const nestedList = item.querySelector('ul');
-
-    if (nestedList) {
-      const firstChild = item.firstChild;
-
-      if (firstChild.nodeType === Node.TEXT_NODE) {
-        const text = firstChild.textContent.trim();
-
-        if (text) {
-          const span = document.createElement('span');
-
-          span.textContent = text;
-          item.insertBefore(span, firstChild);
-          item.removeChild(firstChild);
-        }
-      }
     }
   });
 });
